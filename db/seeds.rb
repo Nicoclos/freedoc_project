f38a59b517cae4a9cdb56bebc3876bb432427908
nicoclos <n.bia@outlook.fr>
2021-10-29 03:42:07 +0200

    v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do 
+    city = City.create(
+    name: Faker::Address.city
+    )
+end
+    
+10.times do 
+    doctor = Doctor.create(
+      first_name: Faker::Name.first_name, 
+      last_name: Faker::Name.last_name, 
+      zip_code: Faker::Address.zip_code,
+     
+    )
+end
+  
+20.times do
+    patient = Patient.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+    )
+end
+
+10.times do
+    Specialty.create(
+        name: Faker::Job.title
+    )
+end
+
+doctor =Doctor.all
+patient = Patient.all
+specialties = Specialty.all
+
+10.times do |i|
+    Appointment.create(date:Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) , 
+    doctor:doctor[rand(0...doctor.length)], 
+    patient:patient[rand(0...patient.length)],
+    )
+end
